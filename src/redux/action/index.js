e17ff617291ea9954195d7098bbf4d2c87b74ede
NoemiP94 <m_emi94@hotmail.it>
2024-05-21 09:38:15 +0200

    fix login

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -10,6 +10,7 @@ export const postLogin = (login) => {
           'Content-Type': 'application/json',
         },
       })
+
       if (res.ok) {
         const data = await res.json()
         console.log(data)
@@ -17,12 +18,13 @@ export const postLogin = (login) => {
           type: POST_LOGIN,
           payload: { token: data.token, role: data.role },
         })
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('role', data.role)
-        localStorage.setItem('isLogged', true)
-        alert("Hai effettuato l'accesso correttamente!")
+        // localStorage.setItem('token', data.token)
+        // localStorage.setItem('role', data.role)
+        // localStorage.setItem('isLogged', true)
+        // alert("Hai effettuato l'accesso correttamente!")
         return data
       } else {
+        // localStorage.setItem('isLogged', false)
         throw new Error('Login is failed!')
       }
     } catch (error) {
