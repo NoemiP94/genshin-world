0a2fd0e70c7ae600ce6dd62018fb60f286a7f960
NoemiP94 <m_emi94@hotmail.it>
2024-05-24 10:08:16 +0200

    update layout

--- src/components/Region.jsx
+++ src/components/Region.jsx
@@ -3,12 +3,13 @@ import ListRegion from './ListRegion'
 
 const Region = () => {
   return (
-    <>
-      <div className="container m-6 w-full">
+    <div>
+      <h2 className="mt-5">Gestione Regioni di Teyvat</h2>
+      <div className="container m-6 w-full flex">
         <CreateRegion />
         <ListRegion />
       </div>
-    </>
+    </div>
   )
 }
 export default Region
