1b6d5e3d03ac47f238b56a988dce99dbf8a34a5d
NoemiP94 <m_emi94@hotmail.it>
2024-06-11 11:45:06 +0200

    add css list region

--- src/components/Region.jsx
+++ src/components/Region.jsx
@@ -267,15 +267,20 @@ const Region = () => {
 
         {/* INIZIO LISTA REGIONI */}
         <div className="w-2/4">
-          <ul role="list" className="divide-y divide-gray-100 ms-5">
-            <p className="text-white">Lista regioni</p>
+          <ul role="list" className="divide-y divide-gray-100 ms-5 px-5">
+            <p className="text-white text-lg">Lista regioni</p>
             {data.content &&
               data.content.map((region) => (
-                <li key={region.id} className="my-3 text-left">
+                <li key={region.id} className="my-3 text-left px-5">
                   <div className="flex justify-between	">
                     <div>
-                      <p className="pt-2">Nome: {region.name}</p>
-                      <p>Tipo di visione: {region.visionType}</p>
+                      <p className="pt-2">
+                        Nome: <span className="italic">{region.name}</span>
+                      </p>
+                      <p className="py-2">
+                        Tipo di visione:{' '}
+                        <span className="italic">{region.visionType}</span>
+                      </p>
                       <Menu
                         as="div"
                         className="relative inline-block text-left"
@@ -300,39 +305,52 @@ const Region = () => {
                                 key={place.id}
                               >
                                 <MenuItem className="flex">
-                                  {/* {({ focus }) => ( */}
-
-                                  <a
-                                  // href="#"
-                                  // className={classNames(
-                                  //   focus
-                                  //     ? 'bg-gray-100 text-white-900'
-                                  //     : 'text-white-700',
-                                  //   'block px-4 py-2 text-sm'
-                                  // )}
-                                  >
-                                    {place.name}
-
-                                    <button
-                                      type="button"
-                                      className="inline-flex w-full justify-center bg-yellow-500 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-600 sm:ml-3 sm:w-auto"
-                                      onClick={() =>
-                                        handleUpdateButton(place, region.id)
-                                      }
-                                    >
-                                      modifica
-                                    </button>
-                                    <button
-                                      type="button"
-                                      className="inline-flex w-full justify-center bg-red-500 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600 sm:ml-3 sm:w-auto"
-                                      onClick={() =>
-                                        handleDeletePlace(place.id)
-                                      }
-                                    >
-                                      elimina
-                                    </button>
+                                  <a className="italic py-2">
+                                    <div className="flex">
+                                      <div className="pe-5">{place.name}</div>
+                                      <div className="flex">
+                                        <button
+                                          type="button"
+                                          className="block text-white"
+                                          onClick={() =>
+                                            handleUpdateButton(place, region.id)
+                                          }
+                                        >
+                                          <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="#facc15"
+                                            className="size-6 me-1"
+                                          >
+                                            <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                                          </svg>
+                                        </button>
+                                        <button
+                                          type="button"
+                                          className="block text-white"
+                                          onClick={() =>
+                                            handleDeletePlace(place.id)
+                                          }
+                                        >
+                                          <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            fill="#dc2626"
+                                            className="size-6 me-1"
+                                            onClick={() => {
+                                              handleDelete(region)
+                                            }}
+                                          >
+                                            <path
+                                              fillRule="evenodd"
+                                              d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                                              clipRule="evenodd"
+                                            />
+                                          </svg>
+                                        </button>
+                                      </div>
+                                    </div>
                                   </a>
-                                  {/* )} */}
                                 </MenuItem>
                               </Transition>
                             ))
@@ -349,35 +367,28 @@ const Region = () => {
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 24 24"
-                            fill="currentColor"
+                            fill="#facc15"
                             className="size-6 me-1"
                           >
                             <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
                           </svg>
-                          Modifica Regione
                         </button>
-                      </div>
-                      <div className="flex my-1">
-                        <div>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="size-6 me-1"
-                            onClick={() => {
-                              handleDelete(region)
-                            }}
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                        <div>
-                          <p>Elimina regione</p>
-                        </div>
+
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="#dc2626"
+                          className="size-6 me-1"
+                          onClick={() => {
+                            handleDelete(region)
+                          }}
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
                       </div>
                     </div>
                   </div>
@@ -387,7 +398,7 @@ const Region = () => {
         </div>
       </div>{' '}
       {/* FINE REGION */}{' '}
-      <div>
+      <div className="mt-10">
         <Place region={region} idPlace={idPlace} />
       </div>
     </div>
