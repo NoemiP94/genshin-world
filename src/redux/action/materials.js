a7df189a2ead0cdb3f68a6e251a23a015964a233
NoemiP94 <m_emi94@hotmail.it>
2024-06-18 11:11:20 +0200

    add post material redux

--- src/redux/action/materials.js
+++ src/redux/action/materials.js
@@ -1,1 +1,29 @@
 export const POST_MATERIAL = 'POST_MATERIAL'
+
+export const postMaterial = (material, token) => {
+  return async (dispatch) => {
+    try {
+      const res = await fetch('http://localhost:3001/material/create', {
+        method: 'POST',
+        body: JSON.stringify(material),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      if (res.ok) {
+        const data = await res.json()
+        console.log(data)
+        dispatch({
+          type: POST_MATERIAL,
+          payload: data,
+        })
+        alert('Materiale salvato con successo')
+      } else {
+        throw new Error('Salvataggio fallito!')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
