ee81b95ffad33a185aaa2c2e32b88eabac5ecbbf
NoemiP94 <m_emi94@hotmail.it>
2024-06-20 12:30:35 +0200

    delete stars in create piece

--- src/components/Artifacts.jsx
+++ src/components/Artifacts.jsx
@@ -200,7 +200,7 @@ const Artifacts = () => {
                   <div className="w-1/4 mt-4 mx-4 flex">
                     <Menu as="div" className="relative inline-block text-left">
                       <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                        Lista località
+                        Lista pezzi
                         <ChevronDownIcon
                           className="-mr-1 h-5 w-5 text-gray-400"
                           aria-hidden="true"
@@ -223,6 +223,29 @@ const Artifacts = () => {
                                   <div className="flex">
                                     <div className="pe-5">{piece.name}</div>
                                     <div className="flex">
+                                      <button
+                                        type="button"
+                                        className="block text-white"
+                                        //   onClick={() =>
+                                        //     handleUpdateButton(place, region.id)
+                                        //   }
+                                      >
+                                        <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          fill="#15803d"
+                                          viewBox="0 0 24 24"
+                                          strokeWidth={1.5}
+                                          stroke="currentColor"
+                                          className="size-8 mx-2"
+                                          //onClick={() => showModalImg(material.id)}
+                                        >
+                                          <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                                          />
+                                        </svg>
+                                      </button>
                                       <button
                                         type="button"
                                         className="block text-white"
--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -146,37 +146,6 @@ const Piece = (artifact) => {
               </div>
             </div>
 
-            <div className="sm:col-span-3 pt-5">
-              <label
-                htmlFor="stars"
-                className="block text-sm font-medium leading-6 text-left"
-              >
-                Stelle
-              </label>
-              <div className="mt-2">
-                <select
-                  id="stars"
-                  name="stars"
-                  autoComplete="stars-name"
-                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                  // value={region.vision}
-                  onChange={(e) => {
-                    setPiece({
-                      ...piece,
-                      stars: e.target.value,
-                    })
-                  }}
-                >
-                  <option>Seleziona una stella</option>
-                  <option>ONE</option>
-                  <option>TWO</option>
-                  <option>THREE</option>
-                  <option>FOUR</option>
-                  <option>FIVE</option>
-                </select>
-              </div>
-            </div>
-
             <div className="mt-6 flex items-center justify-end gap-x-6">
               <button
                 type="reset"
